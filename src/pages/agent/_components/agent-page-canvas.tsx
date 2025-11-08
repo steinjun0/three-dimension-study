b9be5e7a685ef94ec5e33bcec2901e3ed99aa7a2
steinjun0 <1142308@naver.com>
2025-11-08 23:45:41 +0900

    feat: enemy들이 쫓아오도록 구현

--- src/pages/agent/_components/agent-page-canvas.tsx
+++ src/pages/agent/_components/agent-page-canvas.tsx
@@ -3,8 +3,10 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { Physics, RapierRigidBody, RigidBody } from "@react-three/rapier";
 
 import { useRef } from "react";
-import { parseBidirectionalInput } from "../../../utils/keyboard/parse-birdrecional-input";
 import type { BasicMoveKeyboardControlInputs } from "../../../utils/keyboard/basic-move-keyboard-control-inputs";
+import { parseBidirectionalInput } from "../../../utils/keyboard/parse-birdrecional-input";
+
+const ENEMIES_COUNT = 100;
 
 export const AgentPageCanvas = () => {
   const { camera } = useThree();
@@ -14,6 +16,13 @@ export const AgentPageCanvas = () => {
   camera.position.set(0, 100, 0);
 
   const agent = useRef<RapierRigidBody>(null);
+  const enemyRefs = useRef<(RapierRigidBody | null)[]>([]);
+
+  const setEnemyRef = (el: RapierRigidBody | null, index: number) => {
+    if (el) {
+      enemyRefs.current[index] = el;
+    }
+  };
 
   useFrame(() => {
     const { up, down, left, right } = get();
@@ -21,18 +30,37 @@ export const AgentPageCanvas = () => {
     const agentBody = agent.current;
 
     if (agentBody) {
-      agentBody.setTranslation(
-        {
-          x:
-            agentBody.translation().x +
-            parseBidirectionalInput({ negative: left, positive: right }),
-          y: 0,
-          z:
-            agentBody.translation().z +
-            parseBidirectionalInput({ negative: up, positive: down }),
-        },
-        true
-      );
+      const currentAgentTranslation = agentBody.translation();
+
+      //
+      enemyRefs.current.forEach((enemyBody, index) => {
+        if (enemyBody) {
+          // 1. 현재 위치 가져오기
+          const currentEnemyTranslation = enemyBody.translation();
+          const diff = {
+            x: (currentAgentTranslation.x - currentEnemyTranslation.x) * 0.1,
+            y: 0,
+            z: (currentAgentTranslation.z - currentEnemyTranslation.z) * 0.1,
+          };
+          enemyBody.applyImpulse(diff, true);
+        }
+      });
+
+      const isAboveGround = currentAgentTranslation.y > -1;
+      if (isAboveGround && (up || down || left || right)) {
+        agentBody.setTranslation(
+          {
+            x:
+              currentAgentTranslation.x +
+              parseBidirectionalInput({ negative: left, positive: right }),
+            y: 0,
+            z:
+              currentAgentTranslation.z +
+              parseBidirectionalInput({ negative: up, positive: down }),
+          },
+          true
+        );
+      }
     }
   });
 
@@ -52,17 +80,37 @@ export const AgentPageCanvas = () => {
         <RigidBody type="fixed" position={[0, 0, 0]}>
           <mesh>
             <meshStandardMaterial color="grey" />
-            <boxGeometry args={[100, 1, 100]} />
+            <boxGeometry args={[1000, 1, 1000]} />
           </mesh>
         </RigidBody>
 
         {/* agent */}
-        <RigidBody position={[0, 30, 0]} ref={agent}>
+        <RigidBody position={[0, 30, 0]} ref={agent} type="fixed">
           <mesh>
             <meshStandardMaterial color={"orange"} />
             <sphereGeometry args={[1, 32, 16]} />
           </mesh>
         </RigidBody>
+
+        {/* enemies */}
+        <group>
+          {new Array(ENEMIES_COUNT).fill(null).map((_, index) => {
+            return (
+              <RigidBody key={index} ref={(el) => setEnemyRef(el, index)}>
+                <mesh
+                  position={[
+                    Math.floor(index / 10) * 10 - 50,
+                    10,
+                    (index % 10) * 4 - 25,
+                  ]}
+                >
+                  <boxGeometry args={[5, 1, 2]} />
+                  <meshStandardMaterial color={"white"} />
+                </mesh>
+              </RigidBody>
+            );
+          })}
+        </group>
       </Physics>
 
       <OrbitControls enabled={false} />

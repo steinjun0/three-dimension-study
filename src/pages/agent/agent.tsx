e27b76cd174f5cba45efe7587cda66e1a3cef580
steinjun0 <1142308@naver.com>
2025-11-08 23:05:37 +0900

    feat: 키보드 관련 상수 선언

--- src/pages/agent/agent.tsx
+++ src/pages/agent/agent.tsx
@@ -1,22 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { AgentPageCanvas } from "./_components/agent-page-canvas";
 import { KeyboardControls } from "@react-three/drei";
-
-// 액션 이름을 enum으로 정의하는 것이 좋습니다.
-export const Controls = {
-  up: "up",
-  down: "down",
-  left: "left",
-  right: "right",
-} as const;
-
-// 맵 정의
-const keyboardMap = [
-  { name: Controls.up, keys: ["ArrowUp", "KeyW"] },
-  { name: Controls.down, keys: ["ArrowDown", "KeyS"] },
-  { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
-  { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
-];
+import { keyboardMap } from "./_contants/keyboard-control";
 
 export const AgentPage = () => {
   return (
